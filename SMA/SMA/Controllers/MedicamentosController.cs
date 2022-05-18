3a08073f0235a78fc729d11a01612e5285a13df5
Andre Sa <aluno21296@ipt.pt>
2022-05-19 00:59:28 +0100

    Adicionados comportamentos ao código à cerca do manipulamento das fotos.

--- SMA/SMA/Controllers/MedicamentosController.cs
+++ SMA/SMA/Controllers/MedicamentosController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+
 using SMA.Data;
 using SMA.Models;
 
@@ -14,6 +16,7 @@ namespace SMA.Controllers
     {
         // Cria uma instância de acesso à base de dados.
         private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
         public MedicamentosController(ApplicationDbContext context)
         {
@@ -63,17 +66,48 @@ namespace SMA.Controllers
             if (foto == null) { 
                 medicamento.Foto = "FotoDefault.png";
             } else {
-                if (!(foto.ContentType == "image/png" || foto.ContentType == "imagem/jpeg")) {
+                if (!(foto.ContentType == "image/png" || foto.ContentType == "imagem/jpeg")) {  // Se não for um ficheiro de imagem...
                     // Mostra mensagem de erro e devolve o controlo da app à View.
                     ModelState.AddModelError("","Por favor, adicione um ficheiro .png ou .jpg");
                     return View(medicamento);
+                } else {    // Se for ficheiro de imagem...
+                    // Define um nome para a imagem
+                    Guid id = Guid.NewGuid();
+                    string nomeImagem       = medicamento.Id.ToString() + "_" + id.ToString();
+                    string extensaoImagem   = Path.GetExtension(foto.FileName).ToLower();
+                    nomeImagem += extensaoImagem;
+                    medicamento.Foto = nomeImagem;
                 }
             }
 
+            // Validar se os dados providenciados pelo utilizador são bons.
             if (ModelState.IsValid)
             {
-                _context.Add(medicamento);
-                await _context.SaveChangesAsync();
+                try {
+                    // Adiciona os dados do medicamento à base de dados.
+                    _context.Add(medicamento);
+                    await _context.SaveChangesAsync();
+                } catch (Exception) {
+                    // Mensagem de erro.
+                    ModelState.AddModelError("", "Ocorreu um erro. Não foi possível guardar os dados na base de dados.");
+                    return View(medicamento);
+                }
+
+                if (foto != null) {
+                    // Guarda o ficheiro de imagem no disco.
+                    string diretoriaFicheiro = _webHostEnvironment.WebRootPath;
+                    string imagemLocalizacao = Path.Combine(diretoriaFicheiro, "Fotos", medicamento.Foto);
+
+                    // Verifica se a pasta 'Fotos' existe.
+                    if (!Directory.Exists(imagemLocalizacao)) {
+                        Directory.CreateDirectory(imagemLocalizacao);
+                    }
+                    
+                    // Guarda o ficheiro de imagem no disco.
+                    imagemLocalizacao = Path.Combine(imagemLocalizacao, medicamento.Foto);
+                    using var stream = new FileStream(imagemLocalizacao, FileMode.Create);
+                    await foto.CopyToAsync(stream);
+                }
                 return RedirectToAction(nameof(Index));
             }
             return View(medicamento);
@@ -100,11 +134,22 @@ namespace SMA.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Dosagem,Laboratorio,Observacoes,Foto")] Medicamento medicamento)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Dosagem,Laboratorio,Observacoes,Foto")] Medicamento medicamento, IFormFile foto)
         {
             if (id != medicamento.Id)
             {
                 return NotFound();
+            }
+
+            var idAnteriorMedicamento = HttpContext.Session.GetInt32("MedicamentoID");
+
+            if (idAnteriorMedicamento == null) {
+                ModelState.AddModelError("","ERRO!");
+                return View(medicamento);
+            }
+
+            if (idAnteriorMedicamento != medicamento.Id) {
+                return RedirectToAction("Index");
             }
 
             if (ModelState.IsValid)
@@ -153,17 +198,15 @@ namespace SMA.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Medicamentos == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Medicamentos'  is null.");
-            }
             var medicamento = await _context.Medicamentos.FindAsync(id);
-            if (medicamento != null)
-            {
-                _context.Medicamentos.Remove(medicamento);
-            }
-            
             await _context.SaveChangesAsync();
+
+            try {
+                _context.Medicamentos.Remove(medicamento);
+                await _context.SaveChangesAsync();
+            } catch (Exception) {
+            }
+                       
             return RedirectToAction(nameof(Index));
         }
 
